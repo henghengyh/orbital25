0013f8d1b19f2dcf371369b4560a76e717904201
henghengyh <yihenglee6@gmail.com>
2025-05-06 18:50:36 +0800

    [BE] Minor changes to debug (Incomplete)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
