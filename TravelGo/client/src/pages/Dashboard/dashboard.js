cefe6bd59700361d5605e9c2b199df5d856277ed
haobuhaoo <haoyi0509@gmail.com>
2025-06-11 18:50:48 +0800

    [FE] display user itineraries with calender at the side

--- TravelGo/client/src/pages/Dashboard/dashboard.js
+++ TravelGo/client/src/pages/Dashboard/dashboard.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { DayPicker } from 'react-day-picker';
 
 import axiosInstance from "../../utils/axiosInstance";
 import ItineraryCard from "../../components/Cards/intinerarycard";
 
 export default function Dashboard() {
+    const [dateRange, setDateRange] = useState({ form: null, to: null });
     const [itineray, setItinerary] = useState("");
 
     const getAllItinerary = async () => {
-        axiosInstance.get("/itineraries/get-all-itineraries")
+        axiosInstance
+            .get("/itineraries/get-all-itineraries")
             .then((res) => {
                 setItinerary(res.data.itineraries);
             })
@@ -16,17 +19,60 @@ export default function Dashboard() {
             });
     }
 
+    const handleClick = () => { }
+
+    const filterItineraryByDate = async (day) => {
+
+    }
+
+    const handleDayClick = (day) => {
+        setDateRange(day);
+        filterItineraryByDate(day);
+    }
+
+    useEffect(() => {
+        getAllItinerary();
+        return () => { };
+    }, []);
+
     return (
         <div className="start-block">
             <div className="flex gap-7">
                 <div className="flex-1 bg-blue-200">
-                    <div className='grid grid-cols-2 gap-4'>
-                        <ItineraryCard />
-                        <ItineraryCard />
-                        <ItineraryCard />
+                    {itineray.length > 0
+                        ? (
+                            <div className="grid grid-cols-2 gap-4">
+                                {itineray.map((item) => {
+                                    return (
+                                        <ItineraryCard
+                                            key={item._id}
+                                            destination={item.destination}
+                                            startDate={item.startDate}
+                                            endDate={item.endDate}
+                                            numberOfPeople={item.numberOfPeople}
+                                            notes={item.notes}
+                                            onClick={() => handleClick(item)}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        ) : (<>Empty Itinerary</>)}
+                </div>
+
+                <div className="w-[335px]">
+                    <div className="bg-white border border-slate-200 shadow-lg shadow-slate-200/60 rounded-lg">
+                        <div className="p-3">
+                            <DayPicker
+                                navLayout="around"
+                                captionLayout="dropdown-buttons"
+                                mode="range"
+                                selected={dateRange}
+                                onSelect={handleDayClick}
+                                pagedNavigation
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="w-[320px] bg-green-50">calender</div>
             </div>
         </div>
     );
