ffb631686767f20ecf1291409799933c92ecd40f
henghengyh <yihenglee6@gmail.com>
2025-06-18 00:26:53 +0800

    [BE] Implement backend profile settings endpoints, display profile pic at frontend nav bar

--- TravelGo/client/src/components/Cards/profileinfo.js
+++ TravelGo/client/src/components/Cards/profileinfo.js
@@ -1,11 +1,22 @@
 import { getInitials } from "../../utils/helper"
+const REACT_PORT = 3000; //process.env.REACT_APP_API_PORT;
+
+const imageURL = (`http://localhost:${REACT_PORT}`);
 
 export default function ProfileInfo({ user, onLogout }) {
     return (
         <div className="flex items-center justify-center gap-3 w-[180px]">
-            <div className="w-12 h-12 flex items-center justify-center rounded-full font-medium bg-blue-100 cursor-pointer">
-                {getInitials(user?.name)}
-            </div>
+            {user?.profilePhoto ? (
+                <img
+                    src={`${imageURL}${user?.profilePhoto}`}
+                    alt="Profile"
+                    className="w-12 h-12 rounded-full object-cover cursor-pointer"
+                />
+            ) : (
+                <div className="w-12 h-12 flex items-center justify-center rounded-full font-medium bg-blue-100 cursor-pointer">
+                    {getInitials(user?.name)}
+                </div>
+            )}
             <div>
                 <p className="text-base font-medium cursor-pointer">{user?.name}</p>
                 <button className="text-base underline" onClick={onLogout}>Logout</button>
