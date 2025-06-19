e8632ed1dac974cf9bb91382127a9171e32e9b3d
henghengyh <yihenglee6@gmail.com>
2025-06-20 00:01:22 +0800

    [FE] axiosInstance baseURL faulty
    - Deployment to Render need to edit again

--- TravelGo/client/src/utils/axiosInstance.js
+++ TravelGo/client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001",
+    baseURL: "http://localhost:5001", // process.env.REACT_APP_API_URL,
 
     timeout: 60000, // timeout is in ms, not seconds
 
