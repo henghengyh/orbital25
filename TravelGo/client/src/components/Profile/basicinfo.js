a5b8256c5a0af02cee2d3b40079e1ef84de7ec04
henghengyh <yihenglee6@gmail.com>
2025-06-22 00:31:59 +0800

    [FE] Implement profile photo edit functionality with delete function

--- TravelGo/client/src/components/Profile/basicinfo.js
+++ TravelGo/client/src/components/Profile/basicinfo.js
@@ -4,14 +4,14 @@ function basicInfo(user) {
             <h2 className="text-2xl font-bold mb-4">User Profile</h2>
 
             <div className="flex grid grid-cols-8 items-center gap-y-4">
-                <span className="col-span-2">
+                <span className="col-span-8 flex justify-center items-center">
                     <img
                         src={user.profilePhoto ? `http://localhost:3000${user.profilePhoto}` : "/default-avatar.png"}
                         alt="Profile"
-                        className="w-20 h-auto rounded-full object-cover border"
+                        className="w-60 h-60 rounded-full object-cover border"
                     />
                 </span>
-                <span className="col-start-3 col-span-6 font-bold text-2xl"> {user.name}</span>
+                <span className="col-start-4 col-span-2 font-bold items-center text-center text-2xl"> {user.name}</span>
                 <span className="col-start-1 col-span-8 text-center text-sm text-gray-500">
                     {user.profileInfo.bio}
                 </span>
