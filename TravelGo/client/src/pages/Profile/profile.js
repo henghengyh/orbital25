35e5a7fc9c743cf803807cc9a7aa878087d45f43
henghengyh <yihenglee6@gmail.com>
2025-06-21 23:48:47 +0800

    [FE] Implement logout functionality with confirmation modal and improve profile info handling

--- TravelGo/client/src/pages/Profile/profile.js
+++ TravelGo/client/src/pages/Profile/profile.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useUser } from "../../context/UserContext/usercontext";
+import { useAuth } from "../../context/AuthContext/authcontext";
+import { useNavigate } from 'react-router-dom';
+import { logoutUser } from "../../utils/logoutUser";
 import axiosInstance from "../../utils/axiosInstance";
 
 import basicInfo from "../../components/Profile/basicinfo";
@@ -8,73 +11,83 @@ import EditProfileModal from "../../components/Modals/EditProfileModal";
 import EditEmailModal from "../../components/Modals/EditEmailModal";
 import EditPasswordModal from "../../components/Modals/EditPasswordModal";
 import EditSignUpModal from "../../components/Modals/EditSignUpModal";
+import LogoutModal from "../../components/Modals/LogoutModal";
 
 function Profile() {
-  const { user, setUser } = useUser();
-  const [editEmailOpen, setEditEmailOpen] = useState(false);
-  const [editPWOpen, setEditPWOpen] = useState(false);
-  const [editProfileOpen, setEditProfileOpen] = useState(false);
-  const [editSignUpOpen, setEditSignUpOpen] = useState(false);
+    const { user, setUser } = useUser();
+    const [editEmailOpen, setEditEmailOpen] = useState(false);
+    const [editPWOpen, setEditPWOpen] = useState(false);
+    const [editProfileOpen, setEditProfileOpen] = useState(false);
+    const [editSignUpOpen, setEditSignUpOpen] = useState(false);
+    const [logoutOpen, setLogoutOpen] = useState(false);
 
-  if (!user) return <div>Loading...</div>;
+    const { setAuth } = useAuth();
+    const navigate = useNavigate();
 
-  const buttonStyle = "bg-blue-600 hover:bg-blue-700 text-white text-xs font-bold py-2 px-2 w-30 rounded-lg shadow-md transition duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400";
+    if (!user) return <div>Loading...</div>;
 
-  return (
-    <div className="max-w-xl mx-auto mt-10 p-6 bg-white rounded-lg shadow">
-        <EditProfileModal
-            isOpen={editProfileOpen}
-            onClose={() => setEditProfileOpen(null)}
-            onSave={async (form) => {
-                const res = await axiosInstance.post('/users/update-profile', form);
-                setUser(res.data.updatedUser);
-                return res;
-            }}
-            user={user}
-        />
-        <EditEmailModal
-            isOpen={editEmailOpen}
-            onClose={() => setEditEmailOpen(false)}
-            currentEmail={user.email}
-            onEmailUpdated={(newEmail) => setUser((prev) => ({ ...prev, email: newEmail }))}
-        />
-        <EditPasswordModal
-            isOpen={editPWOpen}
-            onClose={() => setEditPWOpen(false)}
-            onSave={async ({ currentPassword, newPassword, confirmPassword }) => {
-                const res = await axiosInstance.post('/users/update-password', {
-                    currentPassword,
-                    newPassword,
-                    confirmPassword
-                });
-                return res;
-            }}
-        />
-        <EditSignUpModal
-            isOpen={editSignUpOpen}
-            onClose={() => setEditSignUpOpen(false)}
-            onSave={async (form) => {
-                const newBool = form.emailSignUp === "Yes" ? true : false;
-                const res = await axiosInstance.post('/users/update-email-signup', { emailSignUp: newBool });
-                
-                setUser(res.data.updatedUser);
-                setEditSignUpOpen(false);
-            }}
-        />
+    const buttonStyle = "bg-blue-600 hover:bg-blue-700 text-white text-xs font-bold py-2 px-2 w-30 rounded-lg shadow-md transition duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400";
+
+    return (
+        <div className="max-w-xl mx-auto mt-10 p-6 bg-white rounded-lg shadow">
+            <EditProfileModal
+                isOpen={editProfileOpen}
+                onClose={() => setEditProfileOpen(null)}
+                onSave={async (form) => {
+                    const res = await axiosInstance.post('/users/update-profile', form);
+                    setUser(res.data.updatedUser);
+                    return res;
+                }}
+                user={user}
+            />
+            <EditEmailModal
+                isOpen={editEmailOpen}
+                onClose={() => setEditEmailOpen(false)}
+                currentEmail={user.email}
+                onEmailUpdated={(newEmail) => setUser((prev) => ({ ...prev, email: newEmail }))}
+            />
+            <EditPasswordModal
+                isOpen={editPWOpen}
+                onClose={() => setEditPWOpen(false)}
+                onSave={async ({ currentPassword, newPassword, confirmPassword }) => {
+                    const res = await axiosInstance.post('/users/update-password', {
+                        currentPassword,
+                        newPassword,
+                        confirmPassword
+                    });
+                    return res;
+                }}
+            />
+            <EditSignUpModal
+                isOpen={editSignUpOpen}
+                onClose={() => setEditSignUpOpen(false)}
+                onSave={async (form) => {
+                    const newBool = form.emailSignUp === "Yes" ? true : false;
+                    const res = await axiosInstance.post('/users/update-email-signup', { emailSignUp: newBool });
+
+                    setUser(res.data.updatedUser);
+                    setEditSignUpOpen(false);
+                }}
+            />
+            <LogoutModal
+                isOpen={logoutOpen}
+                onClose={() => setLogoutOpen(false)}
+                onLogout={() => logoutUser(setAuth, navigate)}
+            />
 
 
-        {basicInfo(user)}
-        <hr className="my-4 border-gray-300" />
-        <DetailedInfo user={user} onEditEmail={() => setEditEmailOpen(true)} onEditPW={() => setEditPWOpen(true)} onEditSignUp={() => setEditSignUpOpen(true)} onEditProfile={() => setEditProfileOpen(true)} />
-        <hr className="my-4 border-gray-300" />
-        <div className="flex justify-center gap-1.5">
-            <button onClick={() => (null)} className={buttonStyle}>
-                Logout
-            </button>
+            {basicInfo(user)}
+            <hr className="my-4 border-gray-300" />
+            <DetailedInfo user={user} onEditEmail={() => setEditEmailOpen(true)} onEditPW={() => setEditPWOpen(true)} onEditSignUp={() => setEditSignUpOpen(true)} onEditProfile={() => setEditProfileOpen(true)} />
+            <hr className="my-4 border-gray-300" />
+            <div className="flex justify-center gap-1.5">
+                <button onClick={() => setLogoutOpen(true)} className={buttonStyle}>
+                    Logout
+                </button>
+            </div>
+            {"message" && <div className="mt-4 text-green-600">{(null)}</div>}
         </div>
-      {"message" && <div className="mt-4 text-green-600">{(null)}</div>}
-    </div>
-  );
+    );
 }
 
 export default Profile;
