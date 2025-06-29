6d8e56848ac147ced49e260e0c22f0da0327eb7b
haobuhaoo <haoyi0509@gmail.com>
2025-06-29 16:40:13 +0800

    [Local testing] switch to local host

--- TravelGo/client/src/utils/axiosInstance.js
+++ TravelGo/client/src/utils/axiosInstance.js
@@ -5,7 +5,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, // "http://localhost:5001",
+    // baseURL: process.env.REACT_APP_API_URL, // "http://localhost:5001",
+    baseURL: "http://localhost:5001",
 
     timeout: 60000, // timeout is in ms, not seconds
 
