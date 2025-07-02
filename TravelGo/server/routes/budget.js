bacc38a95c3a86d8888bc1678d7ca18e87656f1e
haobuhaoo <haoyi0509@gmail.com>
2025-07-02 18:03:06 +0800

    [BE] fix some error for budget

--- TravelGo/server/routes/budget.js
+++ TravelGo/server/routes/budget.js
@@ -11,6 +11,7 @@
 const authenticateToken = require("../middleware/authenticateToken");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const Budget = require("../models/Budget");
 
 const { findBudgetOr404 } = require("../utilities/finder-helper");
@@ -18,12 +19,17 @@ const { findBudgetOr404 } = require("../utilities/finder-helper");
 /** Create a new budget for the itinerary */
 router.post("/", authenticateToken, async (req, res) => {
     try {
-        const { itineraryId, budget } = req.body;
+        const { itineraryIdString, budget } = req.body;
+
+        const itineraryId = new mongoose.Types.ObjectId(itineraryIdString);
+        const existingBudget = await Budget.findOne({ itineraryId });
+        if (existingBudget) return res.status(409).json({ message: "Budget already exists for this itinerary" });
 
         const newBudget = new Budget({ itineraryId, budget });
         await (await newBudget.save()).populate('itineraryId');
         return res.status(200).json({ message: "Budget added" });
     } catch (err) {
+        if (err.code === 11000) return res.status(409).json({ error: "Budget already exists for this itinerary" });
         console.error("Error adding budget:", err);
         return res.status(500).json({ error: "Failed to add budget" });
     }
@@ -32,7 +38,9 @@ router.post("/", authenticateToken, async (req, res) => {
 /** Get budget set for the itinerary */
 router.get("/:itineraryId", authenticateToken, async (req, res) => {
     try {
-        const budget = await Budget.find({ itineraryId: req.params.itineraryId }).select({ budget: 1 });
+        const itineraryId = new mongoose.Types.ObjectId(req.params.itineraryId);
+
+        const budget = await Budget.find({ itineraryId }).select({ budget: 1 });
         return res.status(200).json({ budget });
     } catch (err) {
         return res.status(500).json({ error: true, message: err.message });
