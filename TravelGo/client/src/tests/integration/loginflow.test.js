810105b4cb9dea9d0c41a2d5a7d8f2a3901936f2
haobuhaoo <haoyi0509@gmail.com>
2025-07-14 18:45:50 +0800

    [testing] split into unit and integration testing folders

---
+++ TravelGo/client/src/tests/integration/loginflow.test.js
@@ -0,0 +1,68 @@
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { axiosInstance, renderWithProvAuth } from './test-utils-integ';
+
+describe("Login flow", () => {
+    test("successful (new user) login and redirects to dashboard", async () => {
+        axiosInstance.post.mockResolvedValue({
+            data: {
+                token: "fake-token",
+                user: { _id: '123', name: 'testuser', email: 'unit@test.com' },
+            }
+        });
+        
+        axiosInstance.get.mockResolvedValueOnce({
+            data: {
+                itineraries: [],
+            }
+        });
+
+        renderWithProvAuth();
+        userEvent.type(screen.getByPlaceholderText(/email/i), 'unit@test.com');
+        userEvent.type(screen.getByPlaceholderText(/password/i), 'PASSword123!');
+        userEvent.click(screen.getByRole('button', { name: /log in/i }));
+
+        await waitFor(() => {
+            expect(screen.getByRole('button', { name: /create itinerary/i })).toBeInTheDocument();
+            expect(screen.getByText((text) => text.includes("Click the '+' button to start creating your itinerary"))).toBeInTheDocument();
+        })
+    });
+
+    test("successful (existing) login and redirects to dashboard", async () => {
+        axiosInstance.post.mockResolvedValue({
+            data: {
+                token: "fake-token",
+                user: { _id: '123', name: 'testuser', email: 'unit@test.com' },
+            }
+        });
+        axiosInstance.get.mockResolvedValueOnce({
+            data: {
+                itineraries: [
+                    {
+                        _id: "1",
+                        tripName: "Test Trip",
+                        destination: "Singapore",
+                        imageNumber: 3,
+                        startDate: "2025-07-13",
+                        endDate: "2025-07-13",
+                        numberOfPeople: 1,
+                    },
+                ],
+            }
+        });
+
+        renderWithProvAuth();
+        userEvent.type(screen.getByPlaceholderText(/email/i), 'unit@test.com');
+        userEvent.type(screen.getByPlaceholderText(/password/i), 'PASSword123!');
+        userEvent.click(screen.getByRole('button', { name: /log in/i }));
+
+        await waitFor(() => {
+            expect(screen.getByRole('button', { name: /create itinerary/i })).toBeInTheDocument();
+            expect(screen.getByText(/test trip/i)).toBeInTheDocument();
+            expect(screen.getByText(/singapore/i)).toBeInTheDocument();
+            expect(screen.getByText(/13th jul 2025/i)).toBeInTheDocument();
+            expect(screen.getByText(/number of people: 1/i)).toBeInTheDocument();
+        })
+    })
+});
